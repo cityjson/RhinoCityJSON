39f83b802789a68947fbf1cd8ea7c864c96f5384
jaspervdv <jasper.vaart@live.nl>
2022-11-09 14:45:49 +0100

    Added filter component and changed Idivide c name to IManage

--- RhinoCityJSON/Properties/Resources.Designer.cs
+++ RhinoCityJSON/Properties/Resources.Designer.cs
@@ -83,6 +83,16 @@ namespace RhinoCityJSON.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        public static System.Drawing.Bitmap filtericon {
+            get {
+                object obj = ResourceManager.GetObject("filtericon", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         public static System.Drawing.Bitmap keyselectoricon {
             get {
                 object obj = ResourceManager.GetObject("keyselectoricon", resourceCulture);
--- RhinoCityJSON/RhinoCityJSONReader.cs
+++ RhinoCityJSON/RhinoCityJSONReader.cs
@@ -1756,14 +1756,15 @@ namespace RhinoCityJSON
     public class BuildingToSurfaceInfo : GH_Component
     {
         public BuildingToSurfaceInfo()
-          : base("Information divider", "IDivide",
-              "Divides the building information to surface information format",
+          : base("Information manager", "IManage",
+              "Filters the geometry based on the input semanic data and casts the building information to the surface information format to prepare for the bakery",
               "RhinoCityJSON", "Processing")
         {
         }
 
         protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
         {
+            //pManager.AddBrepParameter("Geometry", "G", "Geometry input", GH_ParamAccess.item);
             pManager.AddTextParameter("Surface Info Keys", "SiK", "Keys of the information output related to the surfaces", GH_ParamAccess.list);
             pManager.AddGenericParameter("Surface Info Vales", "SiV", "Values of the information output related to the surfaces", GH_ParamAccess.tree);
             pManager.AddTextParameter("Object Info Keys", "Oik", "Keys of the Semantic information output related to the objects", GH_ParamAccess.list);
@@ -1772,6 +1773,7 @@ namespace RhinoCityJSON
 
         protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
         {
+            //pManager.AddBrepParameter("Geometry", "G", "Geometry output", GH_ParamAccess.item);
             pManager.AddTextParameter("Merged Surface Info Keys", "mSiK", "Keys of the information output related to the surfaces", GH_ParamAccess.list);
             pManager.AddTextParameter("Merged Surface Info Vales", "mSiV", "Values of the information output related to the surfaces", GH_ParamAccess.item);
         }
@@ -1861,7 +1863,7 @@ namespace RhinoCityJSON
             Parallel.For(0, sBranchCollection.Count, i =>
             {
                 var currentBranch = sBranchCollection[i];
-                string branchBuildingName = currentBranch[0].ToString();
+                string branchBuildingName = currentBranch[nameIdx].ToString();
                 var nPath = new Grasshopper.Kernel.Data.GH_Path(i);
 
                 for (int k = 1; k < bKeys.Count; k++)
@@ -1896,7 +1898,7 @@ namespace RhinoCityJSON
         }
     }
 
-public class Bakery : GH_Component
+    public class Bakery : GH_Component
     {
         public Bakery()
           : base("RCJBakery", "Bakery",
@@ -1936,7 +1938,7 @@ public class Bakery : GH_Component
 
             DA.GetDataList(0, brepList);
             DA.GetDataList(1, keyList);
-            DA.GetDataTree(2, out Grasshopper.Kernel.Data.GH_Structure < Grasshopper.Kernel.Types.IGH_Goo > siTree);
+            DA.GetDataTree(2, out Grasshopper.Kernel.Data.GH_Structure<Grasshopper.Kernel.Types.IGH_Goo> siTree);
 
             if (brepList.Count == 0)
             {
@@ -2068,7 +2070,7 @@ public class Bakery : GH_Component
                     }
                 }
             }
-            
+
             var activeDoc = Rhino.RhinoDoc.ActiveDoc;
 
             // create a new unique master layer name
@@ -2143,7 +2145,7 @@ public class Bakery : GH_Component
                     {
                         lColor = typColor[bType];
                     }
-                    catch 
+                    catch
                     {
                         continue;
                     }
@@ -2238,19 +2240,19 @@ public class Bakery : GH_Component
                 for (int j = 0; j < branchCollection[i].Count; j++)
                 {
                     string fullName = branchCollection[i][j].ToString();
-                    if (j==0)
+                    if (j == 0)
                     {
                         fullName = fullName.Substring(0, fullName.Length - BakerySupport.getPopLength(branchCollection[i][lodIdx].ToString())); // TODO make this smart
                     }
-                    
-                    objectAttributes.SetUserString(keyList[j], fullName);   
+
+                    objectAttributes.SetUserString(keyList[j], fullName);
                 }
 
                 if (bType != "Building")
                 {
                     objectAttributes.LayerIndex = typId[lod][bType];
                 }
-                else if (sType == "None" || surfTypeIdx == -1 )
+                else if (sType == "None" || surfTypeIdx == -1)
                 {
                     objectAttributes.LayerIndex = typId[lod][bType];
                 }
@@ -2291,4 +2293,166 @@ public class Bakery : GH_Component
             get { return new Guid("b2364c3a-18ae-4eb3-aeb3-f76e8a274e18"); }
         }
     }
+
+    public class Filter : GH_Component
+    {
+        public Filter()
+          : base("Filter", "Filter",
+              "Filters the geometry based on the input semanic data and casts the building information to the surface information format to prepare for the bakery",
+              "RhinoCityJSON", "Processing")
+        {
+        }
+
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddTextParameter("Info Keys", "iK", "Keys of the information output", GH_ParamAccess.list);
+            pManager.AddGenericParameter("Info Vales", "iV", "Values of the information output", GH_ParamAccess.tree);
+            pManager.AddTextParameter("Filter Info Keys", "Fik", "Keys of the Semantic information which is used to filter on", GH_ParamAccess.list);
+            pManager.AddTextParameter("Filter Info Value(s)", "FiV", "Value(s) of the semantic information  which is used to filter on", GH_ParamAccess.list);
+            pManager.AddBooleanParameter("Equals/ Not Equals", "==", "Booleans that dictates if the value should be equal, or not equal to filter input value", GH_ParamAccess.item, true);
+
+            pManager[2].Optional = true; // origin is optional
+            pManager[3].Optional = true; // origin is optional
+
+        }
+
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddTextParameter("Merged Surface Info Vales", "mSiV", "Values of the information output related to the surfaces", GH_ParamAccess.item);
+        }
+
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            var keyList = new List<string>();
+            var keyFilter = new List<string>();
+            var keyStrings = new List<string>();
+            bool equals = true;
+
+            DA.GetDataList(0, keyList);
+            DA.GetDataTree(1, out Grasshopper.Kernel.Data.GH_Structure<Grasshopper.Kernel.Types.IGH_Goo> siTree);
+            DA.GetDataList(2, keyFilter);
+            DA.GetDataList(3, keyStrings);
+            DA.GetData(4, ref equals);
+
+            if (keyFilter.Count == 0 && keyStrings.Count != 0)
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Filter Key can not be empty when Filter values is not empty");
+                return;
+            }
+            if (keyFilter.Count != 0 && keyStrings.Count == 0)
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Filter values can not be empty when Filter Key is not empty");
+                return;
+            }
+            if (keyFilter.Count > 1)
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Can only filter on a single key");
+                return;
+            }
+            if (keyFilter.Count == 0 && keyStrings.Count == 0)
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Warning, "No filter input");
+                DA.SetDataTree(0, siTree);
+                return;
+            }
+
+            if (keyList.Count != siTree.Branches[0].Count)
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "The Info keys and values do not comply");
+                return;
+            }
+
+            // get the indx of the filter key
+            int keyIdx = -1;
+
+            for (int i = 0; i < keyList.Count; i++)
+            {
+                if (keyList[i] == keyFilter[0])
+                {
+                    keyIdx = i;
+                }
+            }
+
+            if (keyIdx == -1)
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Filter key can not be found in the info key list");
+                return;
+            }
+
+
+            // find the complying branches
+            var dataTree = new Grasshopper.DataTree<string>();
+            var branchCollection = siTree.Branches;
+
+            if (equals)
+            {
+                for (int i = 0; i < branchCollection.Count; i++)
+                {
+                    for (int j = 0; j < keyStrings.Count; j++)
+                    {
+                        if (keyStrings[j] == branchCollection[i][keyIdx].ToString())
+                        {
+                            var nPath = new Grasshopper.Kernel.Data.GH_Path(i);
+
+                            foreach (var item in branchCollection[i])
+                            {
+                                dataTree.Add(item.ToString(), nPath);
+                            }
+                        }
+                    }
+                }
+            }
+            else
+            {
+                for (int i = 0; i < branchCollection.Count; i++)
+                {
+                    bool found = false;
+                    for (int j = 0; j < keyStrings.Count; j++)
+                    {
+                        if (keyStrings[j] == branchCollection[i][keyIdx].ToString())
+                        {
+                            found = true;
+                            break;
+                        }
+                    }
+                    if (!found)
+                    {
+                        var nPath = new Grasshopper.Kernel.Data.GH_Path(i);
+
+                        foreach (var item in branchCollection[i])
+                        {
+                            dataTree.Add(item.ToString(), nPath);
+                        }
+                    }
+                }
+            }
+
+
+            if (dataTree.BranchCount == 0)
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Warning, "No matching values could be found");
+            }
+
+            DA.SetDataTree(0, dataTree);
+        }
+
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                return RhinoCityJSON.Properties.Resources.filtericon;
+            }
+        }
+
+        /// <summary>
+        /// Each component must have a unique Guid to identify it. 
+        /// It is vital this Guid doesn't change otherwise old ghx files 
+        /// that use the old ID will partially fail during loading.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("b2364c9a-18ae-4eb3-aeb3-f76e8a274e40"); }
+        }
+    }
+
 }
